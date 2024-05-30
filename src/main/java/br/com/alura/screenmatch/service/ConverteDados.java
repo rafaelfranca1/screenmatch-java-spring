c825b10459f30335ffcda2dce63ba351478156b7
rafaelfranca1 <rafaeldefranca750@gmail.com>
2024-05-30 00:13:28 -0300

    fix .gitignore

--- src/main/java/br/com/alura/screenmatch/service/ConverteDados.java
+++ src/main/java/br/com/alura/screenmatch/service/ConverteDados.java
@@ -1,6 +1,5 @@
 package br.com.alura.screenmatch.service;
 
-import br.com.alura.screenmatch.model.DadosSerie;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
